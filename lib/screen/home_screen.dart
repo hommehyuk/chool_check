9ad38653265ce0b19d113f68f0acc15790be7ce2
hommehyuk <bin6827@gmail.com>
2024-03-14 16:43:49 +0900

    지도에 마커 그리기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,13 +20,33 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 15,
   );
   static final double distance = 100;
-  static final Circle circle = Circle(
-    circleId: CircleId('circle'),
+  static final Circle withinDistanceCircle = Circle(
+    circleId: CircleId('withinDistanceCircle'),
     center: companyLatLng,
     fillColor: Colors.blue.withOpacity(0.5),
     radius: distance,
     strokeColor: Colors.blue,
     strokeWidth: 1,
+  );
+  static final Circle notwithinDistanceCircle = Circle(
+    circleId: CircleId('notwithinDistanceCircle'),
+    center: companyLatLng,
+    fillColor: Colors.red.withOpacity(0.5),
+    radius: distance,
+    strokeColor: Colors.red,
+    strokeWidth: 1,
+  );
+  static final Circle checkDoneCircle = Circle(
+    circleId: CircleId('checkDoneCircle'),
+    center: companyLatLng,
+    fillColor: Colors.green.withOpacity(0.5),
+    radius: distance,
+    strokeColor: Colors.green,
+    strokeWidth: 1,
+  );
+  static final Marker marker = Marker(
+    markerId: MarkerId('marker'),
+    position: companyLatLng,
   );
 
   @override
@@ -47,7 +67,8 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 _CustomGoogleMap(
                   initialPosition: initialPosition,
-                  circle: circle,
+                  circle: withinDistanceCircle,
+                  marker: marker,
                 ),
                 _ChoolCheckButton(),
               ],
@@ -101,10 +122,12 @@ class _HomeScreenState extends State<HomeScreen> {
 class _CustomGoogleMap extends StatelessWidget {
   final CameraPosition initialPosition;
   final Circle circle;
+  final Marker marker;
 
   const _CustomGoogleMap({
     required this.initialPosition,
     required this.circle,
+    required this.marker,
     super.key,
   });
 
@@ -118,6 +141,7 @@ class _CustomGoogleMap extends StatelessWidget {
         myLocationEnabled: true,
         myLocationButtonEnabled: false,
         circles: Set.from([circle]),
+        markers: Set.from([marker]),
       ),
     );
   }
